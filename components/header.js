7aab7fce4704d53418034a6a8e771f20dca6ce58
postpreview <secretpics1989@gmail.com>
2026-04-10 10:31:14 +0500

    Update header.js

--- components/header.js
+++ components/header.js
@@ -9,12 +9,14 @@ const headerHTML = `
   <button class="menu-toggle" id="mobile-menu-btn" aria-label="Toggle Menu">☰</button>
 
   <nav id="nav-menu">
+    <a href="/">Home</a>
     <a href="/#tools">Tools</a>
     <a href="/#preview">Preview</a>
     <a href="/#features">Features</a>
     <a href="/#how">How It Works</a>
     <a href="/#faq">FAQ</a>
     <a href="/guides">Guides</a>
+    <a href="/blog">Blog</a>
   </nav>
   <a href="/#preview" class="btn btn-primary">Try Free ↗</a>
 </header>
